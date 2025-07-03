c529631757b3acea37932697a2888bec7500b05b
rodfer0x80 <rodrigolf080@gmail.com>
2025-07-03 18:51:47 +0100

    test for utils.cpp

---
+++ tests/test_utils.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include <filesystem>
+#include "utils.hpp"
+
+namespace fs = std::filesystem;
+
+class EnsureDirectoryExistsTest : public ::testing::Test {
+protected:
+    std::string testDir;
+
+    void SetUp() override {
+        testDir = "./data/test_temp_dir";
+        if (fs::exists(testDir)) {
+            fs::remove_all(testDir);
+        }
+    }
+
+    void TearDown() override {
+        if (fs::exists(testDir)) {
+            fs::remove_all(testDir);
+        }
+    }
+};
+
+TEST_F(EnsureDirectoryExistsTest, CreatesDirectoryIfNotExists) {
+    ASSERT_FALSE(fs::exists(testDir));
+
+    ensure_directory_exists(testDir);
+
+    ASSERT_TRUE(fs::exists(testDir));
+    ASSERT_TRUE(fs::is_directory(testDir));
+}
+
+TEST_F(EnsureDirectoryExistsTest, DoesNothingIfDirectoryAlreadyExists) {
+    fs::create_directory(testDir);
+
+    ASSERT_TRUE(fs::exists(testDir));
+    ASSERT_TRUE(fs::is_directory(testDir));
+
+    ensure_directory_exists(testDir);
+
+    ASSERT_TRUE(fs::exists(testDir));
+    ASSERT_TRUE(fs::is_directory(testDir));
+}
+
